524e96bd48adc522404d3513c675344c0aadc516
Azdra <baptiste.brand2002@gmail.com>
2021-10-02 22:17:52 +0200

    Add dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = (_, argv) => {
   console.log(`Building in ${argv.mode} mode.\n`);
@@ -31,6 +32,7 @@ module.exports = (_, argv) => {
       },
     },
     plugins: [
+      new Dotenv({path: '.env'}),
       new HtmlWebpackPlugin({
         filename: 'index.html',
         template: path.join(__dirname, 'public', 'index.html'),
